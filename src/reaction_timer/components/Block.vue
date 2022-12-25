7989a07f8427c9248ca895154a625bb2562ed3b3
dabin-lee <ldabinn@gmail.com>
2022-12-25 18:26:50 +0900

    Build a Reaction Timer Game

---
+++ src/reaction_timer/components/Block.vue
@@ -0,0 +1,41 @@
+<template>
+    <div v-if="showBlock" class="greenBox" @click="endTimer">click me</div>
+</template>
+<script>
+export default {
+    props: ["delay"],
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTime: 0,
+        };
+    },
+    mounted() {
+        setTimeout(() => {
+            this.showBlock = true;
+            this.startTimer();
+        }, this.delay);
+    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10;
+            }, 10);
+        },
+        endTimer() {
+            clearInterval(this.timer);
+            this.$emit("end", this.reactionTime);
+        },
+    },
+};
+</script>
+<style>
+.greenBox {
+    width: 200px;
+    height: 200px;
+    margin: 100px auto;
+    background-color: green;
+    text-align: center;
+}
+</style>
---
+++ src/reaction_timer/components/Results.vue
@@ -0,0 +1,23 @@
+<template>
+    <p>ReactionTime{{ score }}</p>
+    <p>rank: {{ rank }}</p>
+</template>
+<script>
+export default {
+    props: ["score"],
+    data() {
+        return {
+            rank: null,
+        };
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = "250이하";
+        } else if (this.score < 400) {
+            this.rank = "400이하";
+        } else {
+            this.rank = "그 외";
+        }
+    },
+};
+</script>
